b766992651ca84630a40eeecc387a5fc69c37c1d
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-23 12:51:55 +0530

    Details About Problem

---
+++ MoodAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\t This is Mood Analyser Problem");
+        }
+    }
+}
