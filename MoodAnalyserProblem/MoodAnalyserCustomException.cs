12ffcd213494f293607452cdac631887e8accb74
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-23 15:27:09 +0530

    Refactor: Inform User Mood

---
+++ MoodAnalyserProblem/MoodAnalyserCustomException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyserCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            EMPTY_MESSAGE,
+            NULL_MESSAGE,
+            NO_SUCH_CLASS,
+            NO_SUCH_METHOD,
+            NO_SUCH_CONSTRUCTOR
+        }
+        public readonly ExceptionType type;
+
+        public MoodAnalyserCustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
--- MoodAnalyserProblem/Program.cs
+++ MoodAnalyserProblem/Program.cs
@@ -6,7 +6,11 @@ namespace MoodAnalyserProblem
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("\t\t\t\t\t This is Mood Analyser Problem");
+            MoodAnalyser analyser = new MoodAnalyser("I am in sad Mood");
+            Console.WriteLine(analyser.AnalyserMethod());
+            Console.ReadLine();
         }
     }
 }
